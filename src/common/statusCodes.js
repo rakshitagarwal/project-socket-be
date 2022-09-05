2bb77e283419e7e4646df44d6a039dae6ef8c8c3
zuber khan <zuber.khan@globalvoxinc.com>
2022-09-05 16:23:58 +0530

    changed prettie in setting new add status code for manges in error handling changed main in file for functionaly implemented in status code

---
+++ src/common/statusCodes.js
@@ -0,0 +1,6 @@
+const StatusCodes = {
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_FOUND: 404,
+};
+
+export default StatusCodes;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import logger from "./common/logger.js";
+import createError from "http-errors";
 import express from "express";
 import morgan from "morgan";
-
+import statusCodes from "./common/statusCodes.js";
 const app = express();
 const port = 3000;
-
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    logger.error("information log");
+app.get("/test", (req, res, next) => {
+  try {
+    console.log("test.............");
+    throw new Error("error");
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+app.use(function (err, req, res, next) {
+  res.status(statusCodes.INTERNAL_SERVER_ERROR);
+  res.send(err);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
