94e79734564cf90c5ff535234982eaef4d4fb8d6
zuber khan <zuber.khan@globalvoxinc.com>
2022-09-06 09:33:23 +0530

    added eslint.json and implementes added mocha test case added remove code in index.js added file comman-test file for testing case add user-test case

---
+++ src/common/common-tests.js
@@ -0,0 +1,6 @@
+import assert from "assert";
+describe("Common Test", function () {
+  it("should return the sum in test case", function () {
+    assert.equal(3 + 5, 8);
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import createError from "http-errors";
 import express from "express";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 import statusCodes from "./common/statusCodes.js";
 const app = express();
 const port = 3000;
 app.use(morgan("dev"));
+app.use(bodyParser.json());
 
 app.get("/test", (req, res, next) => {
-  try {
-    console.log("test.............");
-    throw new Error("error");
-  } catch (ex) {
-    next(ex);
-  }
+    try {
+        throw new Error("error");
+    } catch (ex) {
+        next(ex);
+    }
 });
 
 app.use(function (req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
-app.use(function (err, req, res, next) {
-  res.status(statusCodes.INTERNAL_SERVER_ERROR);
-  res.send(err);
+app.use(function (err, req, res) {
+    res.status(statusCodes.INTERNAL_SERVER_ERROR);
+    res.send(err);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
