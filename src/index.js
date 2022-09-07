623e1a14f3b8e79e0e6132a1fe6977d2f570f3c9
zuber khan <zuber.khan@globalvoxinc.com>
2022-09-07 18:30:06 +0530

    created i18n for user 	created locales folder,file and json in country details 	added i18n for internationalize 	implemented in i18n and add some packages 	remove unnecessary code in index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import fs from "fs";
 import { createResponse } from "./common/utilies.js";
 import { env } from "./config/env.js";
 import connectDB from "./config/db.js";
+import i18next from "i18next";
+import Backend from "i18next-fs-backend";
+import middlerware18 from "i18next-http-middleware";
+import cookieParser from "cookie-parser";
 const app = express();
 
 const swaggerDoc = JSON.parse(fs.readFileSync("./openapi.json", "utf8"));
@@ -17,6 +21,20 @@ app.use(morgan(env.LOG_ENV));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cookieParser());
+app.use(middlerware18.handle(i18next));
+app.use(express.json());
+
+i18next
+  .use(Backend)
+  .use(middlerware18.LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    backend: {
+      loadPath: process.cwd() + "/locales/{{lng}}/translation.json",
+    },
+  });
+
 app.use(
   "/docs",
   serve,
@@ -34,16 +52,15 @@ app.use("/", (req, res) => {
   res.status(statusCode).json(response);
 });
 
-app.use(function (req, res, next) {
-  next(createError(helpers.StatusCodes.NOT_FOUND));
-});
-
 app.use(function (err, req, res, next) {
   res.status(helpers.StatusCodes.INTERNAL_SERVER_ERROR);
   res.send(err);
   next();
 });
 
+app.get("/user", (req, res) => {
+  res.send({ message: req.t("testing_master") });
+});
 app.listen(env.PORT, () => {
   connectDB();
   console.log(`listening on http://localhost:${env.PORT}`);
