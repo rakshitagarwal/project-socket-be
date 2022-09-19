641920f789291f29e6a43515f9354f139c0054b8
zuber khan <zuber.khan@globalvoxinc.com>
2022-09-19 11:11:09 +0530

    added valdation for user

--- src/user/user-routes.js
+++ src/user/user-routes.js
@@ -8,12 +8,12 @@ import {
 } from "./../common/constants.js";
 import { login, register, remove, update, get } from "./user-handlers.js";
 import { validateSchema } from "../middleware/validate.js";
-import { registers } from "./../common/validationSchemas.js";
+import { registers,idSchema } from "./../common/validationSchemas.js";
 
 export const userRouter = Router();
 userRouter
   .post(USER_LOGIN, login)
   .post(USER_REGISTER, validateSchema.body(registers), register)
-  .delete(ID_POSTFIX, remove)
+  .delete(ID_POSTFIX,validateSchema.params(idSchema), remove)
   .put(ID_POSTFIX, update)
   .get(USER_PATH_ALLID, get);
