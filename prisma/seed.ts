44b1ba783e90e6999fca446033858abaf5ef4b01
ParmarHetul21 <hetul.d.parmar.20@gmail.com>
2023-06-23 18:49:18 +0530

    fix[seed]: database seed with the admin users and masters
    - new migration added for the timezone
    - seed file created for the admin users and creatig the masters
    - added the default(now()) date
    
    [TBD-414]

---
+++ prisma/seed.ts
@@ -0,0 +1,65 @@
+import { PrismaClient } from "@prisma/client";
+
+async function startSeed() {
+    const prismaClient = new PrismaClient();
+    const roles = await prismaClient.masterRole.findMany({});
+    const productCategory = await prismaClient.masterAuctionCategory.findMany(
+        {}
+    );
+    const auctionCategory = await prismaClient.masterAuctionCategory.findMany(
+        {}
+    );
+    const users = await prismaClient.user.findMany({});
+    if (
+        !roles.length &&
+        !productCategory.length &&
+        !auctionCategory.length &&
+        !users.length
+    ) {
+        await prismaClient.masterRole.createMany({
+            data: [
+                {
+                    title: "Admin",
+                },
+                {
+                    title: "Player",
+                },
+            ],
+        });
+        const roleAdmin = await prismaClient.masterRole.findFirst({
+            where: {
+                title: "Admin",
+            },
+        });
+        await prismaClient.masterProductCategory.create({
+            data: {
+                title: "Mobile & Accessories",
+            },
+        });
+        await prismaClient.masterAuctionCategory.create({
+            data: {
+                title: "English action",
+            },
+        });
+        await prismaClient.user.create({
+            data: {
+                first_name: "admin",
+                last_name: "admin",
+                email: "admin929@yopmail.com",
+                password: "admin@12345",
+                country: "India",
+                role_id: roleAdmin?.id as string,
+            },
+        });
+        return "Db Seeding Compeleted.";
+    }
+    return "Db Already Seeded";
+}
+
+startSeed()
+    .then((msg) => {
+        console.log(msg);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
