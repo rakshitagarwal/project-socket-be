f364502c1aa4359d879c30edb7c668383dffe4f6
zuber khan <zuber.khan@globalvoxinc.com>
2023-07-04 11:24:50 +0530

    add validation schema for landing images

--- src/modules/product/product-schemas.ts
+++ src/modules/product/product-schemas.ts
@@ -6,13 +6,13 @@ const schema = (() => {
     const title = z.string().min(1);
     const description = z.string();
     const product_category_id = z.string().uuid();
-    // const landing_image = z.string().uuid()
+    const landing_image = z.string().uuid()
 
     const ZNewAdd = z.object({
         title: title,
         description: description.optional(),
         product_category_id: product_category_id,
-        // landing_image: landing_image
+        landing_image: landing_image
     });
 
 
