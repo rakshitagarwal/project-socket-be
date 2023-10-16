25a0cc175573647e8ba9eb62dd0a421420602c96
maurya_ankit-07 <ankit.mauraya@globalvoxinc.com>
2023-10-16 15:51:32 +0530

    fix : stop mail functionality

--- src/utils/mail-service.ts
+++ src/utils/mail-service.ts
@@ -3,6 +3,8 @@ import { compile } from "handlebars";
 import { Imail } from "./typing/utils-types";
 import fs from "fs";
 import sgMail from "@sendgrid/mail";
+import nodemailer from "nodemailer";
+import logger from "../config/logger";
 
 /**
  * Set SendGrid API key using the environment variable SENDGRID_API_KEY
@@ -26,27 +28,59 @@ sgMail.setApiKey(env.SENDGRID_API_KEY);
 export async function mailService(props: Imail) {
     const htmlTemplate = fs.readFileSync(`assets/templates/${props.template}`, {encoding: "utf8",});
     const template = compile(htmlTemplate);
-    sgMail.send(
-            {
-                to: props.email,
-                from: env.FROM_EMAIL,
-                subject: props.subject,
-                text: " ",
-                html: template({
-                    userName: props.user_name,
-                    passcode: props.otp,
-                    message: props.message,
-                }),
-            },            
-        )
-        .then(
-            () => 
-            console.log('mail sent successfully', ),
-            (error) => {
-                console.error(error);
-                if (error.response) {
-                    console.error(error.response.body);
-                }
-            }
-        );
+    // TODO: remove this nodemailer
+    const transporter = nodemailer.createTransport({
+        service: env.EMAIL_SERVICE,
+        auth: {
+            user: "globalvox.mteam@gmail.com",
+            pass: env.EMAIL_PASSWORD,
+        },
+        port: env.EMAIL_PORT,
+        host: env.EMAIL_HOST,
+    });
+
+    const mailOptions = {
+        from: "globalvox.mteam@gmail.com",
+        to: props.email,
+        subject: props.subject,
+        text: "",
+        html: template({
+            userName: props.user_name,
+            passcode: props.otp,
+            message: props.message,
+        }),
+    };
+
+    return transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            logger.error(
+                `${env.NODE_ENV} - ${err.name} - ${err.message} - ${err.stack}`
+            );
+        }
+        logger.info(`accepted : ${info.accepted} messageId :${info.messageId}`);
+    });
+    //TODO: Open the send grid when it's goes production
+    // sgMail.send(
+    //         {
+    //             to: props.email,
+    //             from: env.FROM_EMAIL,
+    //             subject: props.subject,
+    //             text: " ",
+    //             html: template({
+    //                 userName: props.user_name,
+    //                 passcode: props.otp,
+    //                 message: props.message,
+    //             }),
+    //         },            
+    //     )
+    //     .then(
+    //         () => 
+    //         console.log('mail sent successfully', ),
+    //         (error) => {
+    //             console.error(error);
+    //             if (error.response) {
+    //                 console.error(error.response.body);
+    //             }
+    //         }
+    //     );
 }
